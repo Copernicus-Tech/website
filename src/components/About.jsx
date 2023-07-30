c4a30c6faa74e00027c2262adbc562934e8b35de
Copernicus-Tech <140296442+Copernicus-Tech@users.noreply.github.com>
2023-07-30 00:53:47 -0500

    Improved Init Loading Time, need to refactor 3JS

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,54 +1,18 @@
-import React, { useState } from "react";
-import { motion, useAnimation } from "framer-motion";
+import React from "react";
 
 import { styles } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
-import { textVariant } from "../utils/motion";
 
-const ServiceCard = ({ index, title, icon }) => {
-  const [isAnimating, setIsAnimating] = useState(false);
-  const controls = useAnimation();
-
-  const animateCard = () => {
-    setIsAnimating(true);
-    controls.start({
-      scale: 1.2,
-      transition: { duration: 0 }, // Set duration to 0 to remove delay
-    });
-  };
-
-  const shrinkCard = () => {
-    setIsAnimating(false);
-    controls.start({
-      scale: 1,
-      transition: { duration: 0 }, // Set duration to 0 to remove delay
-    });
-  };
-
+const ServiceCard = ({ title, icon }) => {
   return (
     <div className="xs:w-[250px] w-full">
-      <motion.div
-        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
-        animate={controls}
-        onTapStart={animateCard}
-        onTapCancel={shrinkCard}
-        onTap={shrinkCard}
-      >
-        <div
-          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
-        >
-          <img
-            src={icon}
-            alt="web-development"
-            className="w-16 h-16 object-contain"
-          />
-
-          <h3 className="text-white text-[20px] font-bold text-center">
-            {title}
-          </h3>
+      <div className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+        <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+          <img src={icon} alt="web-development" className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
         </div>
-      </motion.div>
+      </div>
     </div>
   );
 };
@@ -56,31 +20,23 @@ const ServiceCard = ({ index, title, icon }) => {
 const About = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <div>
         <p className={styles.sectionSubText}>Systematic Overview</p>
         <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
-          <span style={{ fontWeight: "bold", color: "#00ff00" }}>
-            MERN
-          </span>
+          <span style={{ fontWeight: "bold", color: "#00ff00" }}>MERN</span>
           ingful development
         </h2>
-      </motion.div>
+      </div>
 
-      <motion.p
-        variants={textVariant(0)} // Set delay to 0 to remove the fade-in effect
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
-      >
+      <p className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
         The MERN stack is a powerful technology combo for building fast and efficient web apps, using MongoDB for data storage,
         Express.js for server-side processing, React.js for UI building, and Node.js for server-side runtime management.
         This enables businesses to create scalable, responsive web apps that handle complex data and interactions, improving user experience and satisfaction.
-      </motion.p>
+      </p>
 
-      <div
-        className="mt-20 flex flex-wrap gap-10"
-        style={{ marginBottom: "-45px" }}
-      >
+      <div className="mt-20 flex flex-wrap gap-10" style={{ marginBottom: "-45px" }}>
         {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service} />
+          <ServiceCard key={service.title} {...service} />
         ))}
       </div>
     </>
